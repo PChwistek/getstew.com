a6040784f329e6df7ae42cc0fa4bf7b3b4b6a572
Philip Chwistek <mrsportsnerd@gmail.com>
2019-06-10 20:25:35 -0400

    fixed some views and sped up video load

--- components/Video/Video.jsx
+++ components/Video/Video.jsx
@@ -2,18 +2,35 @@ import React from 'react'
 
 import Content from '../Content'
 
-const Video = () => {
+export default class Video extends React.Component {
 
-  return (
-    <Content>
-      <div className="video video__container">
-          <video autoPlay muted loop className="video video__vid">
-              <source src="/static/hermitly_demo_6.mp4" type="video/mp4" />
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: false
+    })
+  }
+
+  render() {
+    const { loading } = this.state
+    return (
+      <Content>
+        <div className="video video__container">
+          <div className={loading ? "loader" : "loader__none" }>
+            <img src={ "/static/spinner.gif" } className="spinner"/>
+          </div>
+          <video autoPlay muted loop className={"video video__vid"}>
+              <source src="https://storage.googleapis.com/hermitly-assets/hermitly_demo_6.mp4" type="video/mp4" />
               Your browser is not supported!
           </video>
-      </div>
-    </Content>
-  )
+        </div>
+      </Content>
+    )
+  }
 }
-
-export default Video
