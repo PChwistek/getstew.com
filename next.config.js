f4b364d656caaee94d3067c72ce74b0c5df8de39
Philip Chwistek <mrsportsnerd@gmail.com>
2020-03-10 17:28:11 -0400

    fixed config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withSass = require('@zeit/next-sass')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withSass({
-  target: 'serverless',
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
