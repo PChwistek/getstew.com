3d08dfb8dd58562fb210edf324c37645fbcf19ad
Philip Chwistek <mrsportsnerd@gmail.com>
2020-05-04 01:05:39 -0400

    new footer style

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,15 +3,52 @@ import Content from '../Content'
 
 export default function footer() {
   return (
-    <Content>
     <div className="footer">
-      <div>
-        <img src="/stew-title.png" className="footer footer__image"/>
-      </div>
-      <div>
-        2019 © Stew, LLC.
-      </div>
+      <Content>
+        <div className='footer__rows'>
+          <div className='footer__row-item'>
+            <div>
+              <div>
+                <img src="/stew-logo-white.png" className="footer__image"/>
+              </div>
+              <div className='footer__company-info'>
+              © 2020 Stew, LLC.
+              </div>
+            </div>
+          </div>
+          <div className='footer__row-item'>
+            <div className='footer__row-item__title'>
+              Product
+            </div>
+            <div className='footer__company-info--link'>
+              Pricing
+            </div>
+            <div className='footer__company-info--link'>
+              FAQ
+            </div>
+            <div className='footer__company-info--link'>
+              Support
+            </div>
+          </div>
+          <div className='footer__row-item'>
+            <div className='footer__row-item__title'>
+              Company
+            </div>
+            <div className='footer__company-info--link'>
+              Privacy Policy
+            </div>
+            <div className='footer__company-info--link'>
+              Terms of Use
+            </div>
+            <div className='footer__company-info--link'>
+              Blog
+            </div>
+            <div className='footer__company-info--link'>
+              Contact
+            </div>
+          </div>
+        </div>
+      </Content>
     </div>
-  </Content>
   )
 }
