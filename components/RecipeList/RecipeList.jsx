43805231c9f8f6975395c2ff79e9a887b7a61580
Philip Chwistek <mrsportsnerd@gmail.com>
2020-10-10 21:52:24 -0400

    added new dashboard

--- components/RecipeList/RecipeList.jsx
+++ components/RecipeList/RecipeList.jsx
@@ -21,6 +21,7 @@ export const RecipeList = ( { config }) => {
 
   function convertListToString(tags) {
     let result = ''
+    if (tags.length === 0) return 'none'
     for (let index = 0; index < tags.length; index++) {
       const theTag = tags[index]
       if (index === tags.length - 1) {
@@ -32,6 +33,28 @@ export const RecipeList = ( { config }) => {
     return result
   }
 
+  function getWindows(config) {
+    const length = config.length
+    if (length === 1) {
+      return '1 window'
+    }
+    return `${length} windows`
+  }
+
+  function getLinks(config) {
+    let total = 0
+    for (let index = 0; index < config.length; index++) {
+      const window = config[index]
+      for (let tabIndex = 0; tabIndex < window.tabs.length; tabIndex++) {
+        total += 1
+      }
+    }
+    if (total === 1) {
+      return '1 tabs'
+    }
+    return `${total} tabs`
+  }
+
   return (
     <div className='recipe-list'>
       <h3> Your Recipes </h3>
@@ -39,9 +62,10 @@ export const RecipeList = ( { config }) => {
         {
           recipes.map( (recipe, index) => (
             <div key={ index } className='recipe-list__item'>
-              <Card style={{ maxWidth: "300px", cursor: 'pointer' }}>
+              <Card style={{ cursor: 'pointer' }}>
                 <CardBody>
-                  <CardTitle> { recipe.name } </CardTitle>
+                  <CardTitle> <h4> { recipe.name } </h4> </CardTitle>
+                  <p>{getWindows(recipe.config)}, {getLinks(recipe.config)}</p>
                 </CardBody>
                 <CardFooter> Tags: { convertListToString(recipe.tags)}</CardFooter>
               </Card>
