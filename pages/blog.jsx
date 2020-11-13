afe9e4fa2d608df8fb7bdaed54c6b5bdd9a6a36a
Philip Chwistek <mrsportsnerd@gmail.com>
2020-11-13 17:55:52 -0500

    added some sstuff to blog

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -4,6 +4,8 @@ import Head from 'next/head'
 import Hero from '../components/Hero'
 import Intro from '../components/Intro'
 import Footer from '../components/Footer'
+import Content from '../components/Content'
+import { Card, CardBody, CardFooter, CardTitle, CardImg } from 'shards-react'
 import "../style.scss"
 
 import { getSortedPostsData } from '../lib/posts'
@@ -32,18 +34,21 @@ export default function Blog({ allPostsData }) {
           slogan="The Stew Blog"
           description="Improving internet productivity"
         />
-        <h2>Blog</h2>
-        <ul>
-           {allPostsData.map(({ id, date, title }) => (
-            <li key={ id }>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
+        <Content>
+          <div className='blog-list'>
+            {allPostsData.map(({ id, date, title }) => (
+              <div key={ id } className='blog-list__item'>
+                <Card style={{ cursor: 'pointer' }}>
+                  <CardImg src='https://images.unsplash.com/photo-1574852859542-1b41217a7815?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2500&q=80' className='blog-list__image'/>
+                  <CardBody>
+                    <CardTitle> { title } </CardTitle>
+                  </CardBody>
+                  <CardFooter> { date } </CardFooter>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </Content>
       </Hero>
   
       <Footer />
